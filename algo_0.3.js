2f21284b64ac87def661fd5e1ace7455a21e2569
Arwa ALZanbaki <105979786+Arwa-ALZanbaki@users.noreply.github.com>
2022-06-09 21:12:25 +0300

    Create algo_0.3.js

---
+++ algo_0.3.js
@@ -0,0 +1,36 @@
+/* 
+  String: Reverse
+  Given a string,
+  return a new string that is the given string reversed
+  var string1 = "abcd";
+  var output = "dcba";
+*/
+
+/********************************************************************/
+/* 
+  Acronyms
+  Create a function that, given a string, returns the string’s acronym 
+  (first letter of each word capitalized). 
+  Do it with .split first if you need to, then try to do it without
+*/
+
+const str1 = "object oriented programming";
+const expected1 = "OOP";
+
+// The 4 pillars of OOP
+const str2 = "abstraction polymorphism inheritance encapsulation";
+const expected2 = "APIE";
+
+const str3 = "software development life cycle";
+const expected3 = "SDLC";
+
+// Bonus: ignore extra spaces
+const str4 = "  global   information tracker    ";
+const expected4 = "GIT";
+
+/**
+ * Turns the given str into an acronym.
+ * @param {string} str A string to be turned into an acronym.
+ * @returns {string} The acronym.
+ */
+function acronymize(str) {}
