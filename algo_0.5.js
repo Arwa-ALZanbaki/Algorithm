3b88dd88d3d014eda15c063dbd36b4a4519e7d31
Arwa ALZanbaki <105979786+Arwa-ALZanbaki@users.noreply.github.com>
2022-06-09 21:30:28 +0300

    Create algo_0.5.js

---
+++ algo_0.5.js
@@ -0,0 +1,13 @@
+/*
+    Given array that has arrays of integers, detect the maximum number in each of those arrays and return an array of the maximum numbers
+    for example given the below array
+    const arr = [
+        [3,4,5,6],
+        [10,20,30],
+        [7,8,9]
+    ];
+    expected output
+    [6,30,9]
+    bonus, add the maximum value to the begining of the array so the result become
+    [30,6,9]
+*/
