7d288354ad8108e07aacd18af503343386358aaf
Arwa ALZanbaki <105979786+Arwa-ALZanbaki@users.noreply.github.com>
2022-06-19 09:25:32 +0300

    Create algo_5.1.js

---
+++ algo_5.1.js
@@ -0,0 +1,57 @@
+/* 
+  Given a string that may have extra spaces at the start and the end,
+  return a new string that has the extra spaces at the start and the end trimmed (removed)
+  do not remove any other spaces.
+*/
+str[i] !== " "
+startIndex
+lastIndex
+const str1 = "   hello world     ";
+const expected1 = "hello world";
+
+/**
+ * Trims any leading or trailing white space from the given str.
+ * @param {string} str
+ * @returns {string} The given string with any leading or trailing white space
+ *    stripped.
+ */
+function trim(str) {}
+
+/*****************************************************************************/
+ /*String: Rotate String
+  Create a standalone function that accepts a string and an integer,
+  and rotates the characters in the string to the right by that given
+  integer amount.
+*/
+
+const str = "Hello World";
+
+const rotateAmnt1 = 0;
+const expected1 = "Hello World";
+
+const rotateAmnt2 = 1;
+const expected2 = "dHello Worl";
+
+const rotateAmnt3 = 2;
+const expected3 = "ldHello Wor";
+
+const rotateAmnt4 = 4;
+const expected4 = "orldHello W";
+
+const rotateAmnt5 = 13;
+const expected5 = "ldHello Wor";
+/* 
+Explanation: this is 2 more than the length so it ends up being the same
+as rotating it 2 characters because after rotating every letter it gets back
+to the original position.
+*/
+
+/**
+ * Rotates a given string's characters to the right by the given amount,
+ * wrapping characters to the beginning.
+ * @param {string} str
+ * @param {number} amnt The amount of characters in the given str to rotate to the
+ *    right.
+ * @returns {string} The string rotated by the given amount.
+ */
+function rotateStr(str, amnt) {}
