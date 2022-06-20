1ddfbfe99a74edaeb262fc900a08893969b341de
Arwa ALZanbaki <105979786+Arwa-ALZanbaki@users.noreply.github.com>
2022-06-20 09:22:56 +0300

    Create algo_5.2.js

---
+++ algo_5.2.js
@@ -0,0 +1,70 @@
+/* 
+  Missing Value
+  You are given an array of length N that contains, in no particular order,
+  integers from 0 to N . One integer value is missing.
+  Quickly determine and return the missing value.
+*/
+
+const nums1 = [3, 0, 1];
+const expected1 = 2;
+
+const nums2 = [3, 0, 1, 2];
+const expected2 = null;
+// Explanation: nothing is missing
+
+/* 
+  Bonus: now the lowest value can now be any integer (including negatives),
+  instead of always being 0. 
+*/
+
+const nums3 = [2, -4, 0, -3, -2, 1];
+const expected3 = -1;
+
+const nums4 = [5, 2, 7, 8, 4, 9, 3];
+const expected4 = 6;
+
+/**
+ * Determines what the missing int is in the given unordered array of ints
+ *    which spans from 0 to N where only one int is missing. With this missing
+ *    int, a consecutive sequence of ints could be formed from the array.
+ * Bonus: Given ints can span from N to M (start and end anywhere).
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {Array<number>} unorderedNums
+ * @returns {number|null} The missing integer needed to be able to form an unbroken
+ *    consecutive set of integers from the given array or null if none is missing.
+ */
+function missingValue(unorderedNums) {}
+
+// ---------------------------------------------------------------------------------------------------
+
+/* 
+  Given a SORTED array of integers, dedupe the array 
+  Because array elements are already in order, all duplicate values will be grouped together.
+  Ok to use a new array
+  Bonus: do it in O(n) time (no nested loops, new array ok)
+  Bonus: Do it in-place (no new array)
+  Bonus: Do it in-place in O(n) time and no new array
+  Bonus: Keep it O(n) time even if it is not sorted
+*/
+
+const nums1 = [1, 1, 1, 1];
+const expected1 = [1];
+
+const nums2 = [1, 1, 2, 2, 3, 3];
+const expected2 = [1, 2, 3];
+
+const nums3 = [1, 1, 2, 3, 3, 4];
+const expected3 = [1, 2, 3, 4];
+
+const nums4 = [1, 1];
+const expected4 = [1];
+
+/**
+ * De-dupes the given sorted array.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {Array<number>} nums
+ * @returns {Array<number>} The given array deduped.
+ */
+function dedupeSorted(nums) {}
